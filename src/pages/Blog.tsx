3daa336c13b61b1915ddf82533619e4944413ad6
mesren2 <76926458+mesren2@users.noreply.github.com>
2025-04-29 16:54:55 -0600

    Create Blog.tsx

---
+++ src/pages/Blog.tsx
@@ -0,0 +1,8 @@
+export default function Blog() {
+  return (
+    <div className="p-6 text-white">
+      <h1 className="text-4xl font-bold mb-4">Blog</h1>
+      <p className="text-lg">Welcome to the blog! Posts coming soon.</p>
+    </div>
+  );
+}
